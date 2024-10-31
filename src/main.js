d724bb02c679eb6c1621e3d91522f8920f1060dd
Vinicius P Stormoski <vinicius.stormoski@conectanuvem.com.br>
2024-10-31 19:39:56 -0300

    feat: firebase - login implementation

--- src/main.js
+++ src/main.js
@@ -1,11 +1,12 @@
 import './assets/main.css'
+import { initialApp } from '@/helpers/firebase'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
-
+initialApp()
 const app = createApp(App)
 
 app.use(createPinia())
