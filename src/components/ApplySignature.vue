990c30e254dfdf0691f0e158a1207e96fd9086d9
Vineh <vinehsp@gmail.com>
2024-12-24 23:02:45 -0300

    refact: add fields for signature informations and fix apply method

--- src/components/ApplySignature.vue
+++ src/components/ApplySignature.vue
@@ -1,63 +1,180 @@
 <template>
-  <div>
-    <h2>Olá, {{ name }}!</h2>
-    <p>E-mail: {{ primaryEmail }}</p>
-
-    <h3>Configuração de Assinatura de E-mail</h3>
-    <form @submit.prevent="applySignature">
-      <div>
-        <label for="signature">Assinatura:</label>
-        <div>
-          <div
-            style="
-              width: 600px;
-              margin: 0 auto;
-              padding: 20px;
-              font-family: Arial, sans-serif;
-            "
-          >
-            <div style="float: left">
-              <img
-                :src="photo"
-                alt="Foto do Usuário"
-                style="
-                  max-width: 100%;
-                  height: auto;
-                  border-radius: 50%;
-                  margin-right: 10px;
-                  vertical-align: middle;
-                "
-                width="50"
-              />
+  <div
+    style="width: 100vw; display: flex; flex-direction: column; padding: 20px"
+  >
+    <div
+      style="display: flex; margin-bottom: 10px; justify-content: space-between"
+    >
+      <div
+        style="
+          display: flex;
+          align-items: center;
+          gap: 10px;
+          margin-bottom: 10px;
+        "
+      >
+        <img
+          :src="formData.photo"
+          alt="user photo"
+          width="55"
+          style="border-radius: 50%"
+        />
+        <h2>Olá, {{ formData.user_name }}!</h2>
+      </div>
+      <button @click="logout()">Logout</button>
+    </div>
+    <form style="display: flex; gap: 10px">
+      <input type="radio" value="models" v-model="option" />
+      <label for="models">Utilizar modelos</label>
+      <input type="radio" value="html" v-model="option" />
+      <label for="html">Utilizar html</label>
+    </form>
+    <div v-if="option == 'models'">
+      <h3 style="margin-top: 10px">Modelos de Assinatura de E-mail</h3>
+      <div class="models-buttons">
+        <button @click="setSignatureChoiced('signatureOne')">Modelo 1</button>
+        <button @click="setSignatureChoiced('signatureTwo')">Modelo 2</button>
+      </div>
+      <div class="container">
+        <div class="form-container">
+          <form style="display: flex; gap: 10px; margin-bottom: 20px">
+            <input type="radio" value="primary" v-model="userInfoTab" />
+            <label for="models">Informações principais</label>
+            <input type="radio" value="secondary" v-model="userInfoTab" />
+            <label for="html">Outras informações</label>
+          </form>
+          <form @submit.prevent="applySignature">
+            <div v-if="userInfoTab == 'primary'">
+              <div class="form-group">
+                <label for="email">Email:</label>
+                <input
+                  type="email"
+                  id="email"
+                  v-model="formData.email"
+                  required
+                />
+                <span v-if="errors.email">{{ errors.email }}</span>
+              </div>
+              <div class="form-group">
+                <label for="user_name">Nome:</label>
+                <input
+                  type="text"
+                  id="user_name"
+                  v-model="formData.user_name"
+                  required
+                />
+                <span v-if="errors.name">{{ errors.name }}</span>
+              </div>
+              <div class="form-group">
+                <label for="address">Endereço:</label>
+                <input type="text" id="address" v-model="formData.address" />
+              </div>
+              <div class="form-group">
+                <label for="phone">Telefone:</label>
+                <input type="tel" id="phone" v-model="formData.phone" />
+              </div>
+              <div class="form-group">
+                <label for="department">Departamento:</label>
+                <input
+                  type="text"
+                  id="departament"
+                  v-model="formData.departament"
+                />
+              </div>
+              <div class="form-group">
+                <label for="enterprise_phone">Telefone comercial:</label>
+                <input
+                  type="text"
+                  id="enterprise_phone"
+                  v-model="formData.enterprise_phone"
+                />
+              </div>
             </div>
-            <div style="float: left; margin-left: 10px; vertical-align: middle">
-              <p style="font-weight: bold">{{ name }}</p>
-              <p>{{ primaryEmail }}</p>
-              <p>
-                Telefone:
-                <span style="font-style: italic">{{
-                  phoneNumbers.mobile ? phoneNumbers.mobile : phoneNumbers.work
-                }}</span>
-              </p>
+            <div v-else>
+              <div class="form-group">
+                <label for="enterprise_name">Nome da empresa:</label>
+                <input
+                  type="text"
+                  id="enterprise_name"
+                  v-model="formData.enterprise_name"
+                />
+              </div>
+              <div class="form-group">
+                <label for="website">Site:</label>
+                <input type="text" id="website" v-model="formData.site" />
+              </div>
+              <div class="form-group">
+                <label for="enterprise_logo">Logo:</label>
+                <input
+                  type="text"
+                  id="enterprise_logo"
+                  v-model="formData.enterprise_logo"
+                />
+              </div>
+              <div class="form-group">
+                <label for="instagram">Instagram:</label>
+                <input
+                  type="text"
+                  id="instagram"
+                  v-model="formData.instagram"
+                />
+              </div>
+              <div class="form-group">
+                <label for="facebook">Facebook:</label>
+                <input type="text" id="facebook" v-model="formData.facebook" />
+              </div>
+              <div class="form-group">
+                <label for="twitter">Twitter:</label>
+                <input type="text" id="twitter" v-model="formData.twitter" />
+              </div>
+              <div class="form-group">
+                <label for="linkedin">Linkedin:</label>
+                <input type="text" id="linkedin" v-model="formData.linkedin" />
+              </div>
+              <div class="form-group">
+                <label for="youtube">Youtube:</label>
+                <input type="text" id="youtube" v-model="formData.youtube" />
+              </div>
             </div>
-            <div style="clear: both"></div>
-            <div style="border-top: 1px solid #ccc; padding-top: 20px">
-              <p style="font-size: smaller"></p>
-            </div>
+            <button type="submit">Aplicar Assinatura</button>
+          </form>
+        </div>
+        <div class="preview-container">
+          <h3>Pré-visualização da Assinatura</h3>
+          <div class="signature">
+            <span v-html="generateSignature"></span>
           </div>
         </div>
-        <textarea id="signature" v-model="signature" required></textarea>
       </div>
-      <button type="submit">Aplicar Assinatura</button>
-    </form>
-    <button>Logout</button>
+    </div>
+    <div v-else class="container">
+      <div style="display: flex; flex-direction: column">
+        <textarea
+          v-model="signatureInHtml"
+          name="signatureHtml"
+          style="width: 50vw"
+          id=""
+          cols="30"
+          rows="10"
+        ></textarea>
+        <button @click="applySignature()">Aplicar Assinatura</button>
+      </div>
+      <div class="preview-container">
+        <h3>Pré-visualização da Assinatura</h3>
+        <div class="signature">
+          <span v-html="generateSignature"></span>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script setup>
 import axios from 'axios'
-import { ref, onMounted } from 'vue'
+import { ref, onMounted, reactive, computed } from 'vue'
 import { useRoute, useRouter } from 'vue-router'
+
+import { signatureModels } from '@/signatureModels/signatures'
 
 const route = useRoute()
 const router = useRouter()
@@ -66,6 +183,70 @@ const name = ref('')
 const primaryEmail = ref('')
 const photo = ref('')
 const phoneNumbers = ref({})
+
+const option = ref('models')
+const userInfoTab = ref('primary')
+const signatureInHtml = ref('')
+
+const formData = reactive({
+  photo: '',
+  user_name: '',
+  departament: '',
+  email: '',
+  address: '',
+  website: '',
+  enterprise_logo: '',
+  enterprise_name: '',
+  enterprise_phone: '',
+  facebook: '',
+  instagram: '',
+  twitter: '',
+  linkedin: '',
+  youtube: '',
+  site: '',
+})
+
+const placeholders = [
+  'PHOTO',
+  'USER_NAME',
+  'DEPARTAMENT',
+  'EMAIL',
+  'ADDRESS',
+  'SITE',
+  'ENTERPRISE_LOGO',
+  'ENTERPRISE_NAME',
+  'ENTERPRISE_PHONE',
+  'FACEBOOK',
+  'INSTAGRAM',
+  'TWITTER',
+  'YOUTUBE',
+  'LINKEDIN',
+  'PHONE',
+  'ENTERPRISE_PHONE',
+]
+
+const errors = reactive({
+  email: null,
+  name: null,
+})
+
+function setSignatureChoiced(key) {
+  signature.value = signatureModels[key]
+}
+
+const generateSignature = computed(() => {
+  let signatureHtml = signature.value
+
+  placeholders.forEach(placeholder => {
+    const value = formData[placeholder.toLowerCase()]
+    signatureHtml = signatureHtml.replace(
+      new RegExp(placeholder, 'g'),
+      value || placeholder,
+    )
+  })
+
+  return signatureHtml
+})
 
 async function getUserInfos(token) {
   await axios
@@ -83,17 +264,17 @@ async function getUserInfos(token) {
     .catch(error => {
       console.log('err: ', error)
       if (error.status === 401) {
-        localStorage.removeItem('gtoken')
+        logout()
       }
     })
 }
 
 function setUser(data) {
-  primaryEmail.value = data?.emailAddresses?.find(
+  formData['email'] = data?.emailAddresses?.find(
     el => el?.metadata?.primary,
   )?.value
-  name.value = data?.names?.[0]?.displayName
-  photo.value = data?.photos?.find(el => el?.default)?.url
+  formData['user_name'] = data?.names?.[0]?.displayName
+  formData['photo'] = data?.photos?.find(el => el?.default)?.url
 
   const numbers = data?.phoneNumbers || []
   phoneNumbers.value = numbers.reduce((acc, phoneNumber) => {
@@ -102,29 +283,29 @@ function setUser(data) {
   }, {})
 }
 
-const signatureHTML = ref(`
-    <div style="width:600px; margin:0 auto; padding:20px; font-family:Arial, sans-serif;">
-  <div style="float:left;">
-    <img :src="${photo.value}" alt="Foto do Usuário" style="max-width:100%; height:auto; border-radius:50%; margin-right:10px; vertical-align:middle;" width="50">
-  </div>
-  <div style="float:left; margin-left:10px; vertical-align:middle;">
-    <p style="font-weight:bold;">${name.value}</p>
-    <p>${primaryEmail.value}</p>
-    <p>Telefone: <span style="font-style:italic;">${phoneNumbers.value.mobile ? phoneNumbers.value.mobile : phoneNumbers.value.work}</span></p>
-  </div>
-  <div style="clear:both;"></div>
-  <div style="border-top:1px solid #ccc; padding-top:20px;">
-    <p style="font-size:smaller;"></p>
-  </div>
-</div>
-  `)
+// const signatureHTML = ref(`
+//   <div style="width:600px; margin:0 auto; padding:20px; font-family:Arial, sans-serif;">
+//     <div style="float:left;">
+//       <img :src="${photo.value}" alt="Foto do Usuário" style="max-width:100%; height:auto; border-radius:50%; margin-right:10px; vertical-align:middle;" width="50">
+//       </div>
+//       <div style="float:left; margin-left:10px; vertical-align:middle;">
+//         <p style="font-weight:bold;">${name.value}</p>
+//         <p>${primaryEmail.value}</p>
+//         <p>Telefone: <span style="font-style:italic;">${phoneNumbers.value.mobile ? phoneNumbers.value.mobile : phoneNumbers.value.work}</span></p>
+//         </div>
+//         <div style="clear:both;"></div>
+//         <div style="border-top:1px solid #ccc; padding-top:20px;">
+//           <p style="font-size:smaller;"></p>
+//           </div>
+//           </div>
+//           `)
 
 async function applySignature() {
   const gtoken = localStorage.getItem('gtoken')
   const requestBody = {
-    signature: signatureHTML.value,
+    signature: generateSignature.value,
     access_token: gtoken,
-    email: primaryEmail.value,
+    email: formData.email,
   }
   const url = import.meta.env.VITE_API_URL || 'http://localhost:5000'
 
@@ -145,6 +326,11 @@ async function applySignature() {
     })
 }
 
+function logout() {
+  localStorage.removeItem('gtoken')
+  router.push('/login')
+}
+
 onMounted(async () => {
   const gtoken = route.query.token || localStorage.getItem('gtoken')
   if (!gtoken) {
@@ -155,3 +341,59 @@ onMounted(async () => {
   if (!primaryEmail.value) await getUserInfos(gtoken)
 })
 </script>
+<style scoped>
+.container {
+  display: flex;
+  width: 100%;
+  margin: 20px auto;
+}
+
+.form-container,
+.preview-container {
+  width: 80%;
+  padding: 20px;
+  border: 1px solid #ccc;
+}
+
+.form-group {
+  margin-bottom: 10px;
+}
+
+.form-group label {
+  display: block;
+  margin-bottom: 5px;
+}
+
+.form-group input {
+  width: 100%;
+  padding: 8px;
+  border: 1px solid #ccc;
+  box-sizing: border-box;
+}
+
+.signature {
+  border: 1px dashed #ccc;
+  padding: 10px;
+}
+
+.signature a {
+  color: blue;
+  text-decoration: underline;
+}
+
+.signature p {
+  margin: 5px 0;
+}
+
+.models-buttons {
+  display: flex;
+  gap: 10px;
+  margin-top: 15px;
+}
+
+button {
+  width: 130px;
+  height: 25px;
+  cursor: pointer;
+}
+</style>
